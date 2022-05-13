b42ca02d28819f7b004e61e43c0ce231f08bcd62
Andressa Reis Araujo <andressareis98@outlook.com>
2022-05-12 23:51:03 -0300

    feat: filter itens

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,11 +1,36 @@
 import itens from './itens.json'
 import { Item } from './Item'
 import styles from './Itens.module.scss'
+import { useEffect, useState } from 'react'
 
-export function Itens() {
+interface Props{
+    busca: string,
+    filtro: number | null,
+    ordenador: string
+}
+
+export function Itens({busca, filtro, ordenador} : Props) {
+    const [lista, setLista] = useState(itens)
+
+    const testaBusca = (title: string) =>{
+        const regex = new RegExp(busca, 'i')
+        return regex.test(title)
+    }
+
+    const testaFiltro = (id: number) =>{
+        if(filtro !== null) return filtro === id
+        return true
+    }
+
+
+    useEffect(() => {
+        const novaLista = itens.filter(item => testaBusca(item.title) && testaFiltro(item.category.id))
+        setLista(novaLista)
+    },[busca, filtro])
+
     return(
         <div className={styles.itens}>
-            {itens.map(item => (
+            {lista.map(item => (
                 <Item key={item.id} {...item}/>
             ))}
         </div>
